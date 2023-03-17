99ceb3137e6a371607a849289f69384d3b784779
linda <lindaloay79>
2023-03-17 20:28:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/activity1/Note.java
@@ -0,0 +1,78 @@
+//package com.example.activity1;
+//
+//
+//import com.google.firebase.Timestamp;
+//
+//public class Note {
+//    String title;
+//    String content;
+//    Timestamp timestamp;
+//
+//
+//    public Note() {
+//
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
+//
+//    public Timestamp getTimestamp() {
+//        return timestamp;
+//    }
+//
+//    public void setTimestamp(Timestamp timestamp) {
+//        this.timestamp = timestamp;
+//    }
+//}
+
+//package easy.tuto.notespro;
+package com.example.activity1;
+
+
+import com.google.firebase.Timestamp;
+
+public class Note {
+    String title;
+    String content;
+    Timestamp timestamp;
+
+    public Note() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+}
